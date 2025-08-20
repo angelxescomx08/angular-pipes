be3e7791297e065168e04627b3643249ee344eb2
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-20 11:40:59 -0600

    Enhance basic-page component to include multiple date formats using Angular's DatePipe, and implement a ticking date effect for real-time updates.

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,14 +1,31 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
+  styleUrl: './basic-page.component.css',
 })
 export class BasicPageComponent {
-  nameLower = signal("angel")
-  nameUpper = signal("ANGEL")
-  fullName = signal("AnGeL")
+  nameLower = signal('angel');
+  nameUpper = signal('ANGEL');
+  fullName = signal('AnGeL');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanUp) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanUp(() => {
+      clearInterval(interval);
+    });
+  });
 }
