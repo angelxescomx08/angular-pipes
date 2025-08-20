20e1fcce3c54f4be1df9287202c73d2068d12e72
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-20 11:28:08 -0600

    Update numbers-page component to display various Angular pipes with a new HTML structure and integrate necessary common pipes for improved data presentation.

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
-
+  totalSales = signal(1324433.5567)
+  percent = signal(0.4856)
 }
