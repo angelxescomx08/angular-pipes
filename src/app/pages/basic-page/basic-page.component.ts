24f117b38c10899fc12008e57105abe0e35440bd
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-20 10:51:35 -0600

    Refactor basic-page component to display Angular pipes with updated HTML structure and integrate necessary Angular common pipes.

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
-
+  nameLower = signal("angel")
+  nameUpper = signal("ANGEL")
+  fullName = signal("AnGeL")
 }
