cbfa37d9cde527a865f0240553259dc3a4d7bed7
José Ángel Hernández Radilla <jose.angel.hdz.rda@gmail.com>
2025-08-20 10:07:05 -0600

    Add Tailwind CSS and DaisyUI integration, create basic routing and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes básicos',
+    loadComponent: () =>
+      import('./pages/basic-page/basic-page.component').then(
+        (m) => m.BasicPageComponent
+      ),
+  },
+  {
+    path: 'numbers',
+    title: 'Numbers pipes',
+    loadComponent: () =>
+      import('./pages/numbers-page/numbers-page.component').then(
+        (m) => m.NumbersPageComponent
+      ),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comunes',
+    loadComponent: () =>
+      import('./pages/uncommon-page/uncommon-page.component').then(
+        (m) => m.UncommonPageComponent
+      ),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes personalizados',
+    loadComponent: () =>
+      import('./pages/custom-page/custom-page.component').then(
+        (m) => m.CustomPageComponent
+      ),
+  },
+  {
+    path: "**",
+    redirectTo: "basic"
+  }
+];
